edb474e6953fb29b65e1611824ab612fc77be54e
Volodymyr <volodyaruzhak@gmail.com>
2024-06-24 17:57:17 -0400

    updated questions.js

--- backend/api/questions.js
+++ backend/api/questions.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const cors = require('cors'); // Import the cors package
 const { client, connectDB } = require('./connectDB');
 
 const app = express();
 app.use(express.json());
+app.use(cors()); // Use the cors middleware
 
 app.get('/questions', async (req, res) => {
     await connectDB();
