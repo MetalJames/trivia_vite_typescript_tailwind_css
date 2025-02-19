4747d82e28ea40c5441c16a47878a5514615e4ad
Volodymyr <volodyaruzhak@gmail.com>
2025-02-19 13:28:28 -0500

    -Fix: switched to the render from local fetch.

--- frontend/src/services/fetchMongo.tsx
+++ frontend/src/services/fetchMongo.tsx
@@ -1,7 +1,7 @@
 import axios from "axios";
 
-export const BASE_URL = "http://localhost:8000/questions";
-// export const BASE_URL = "https://trivia-vite-typescript-tailwind-css-eje3.onrender.com/questions";
+// export const BASE_URL = "http://localhost:8000/questions";
+export const BASE_URL = "https://trivia-vite-typescript-tailwind-css-eje3.onrender.com/questions";
 
 export const fetchFromAPI = async (url: string) => {
     try {
